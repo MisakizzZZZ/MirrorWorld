1e6d4cecff701b5627a1ec44a842e31255ea8caf
MisakizzZZZ <401058751@qq.com>
2025-03-14 00:54:57 -0700

    save

---
+++ Assets/Scripts/AutoTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AutoTrigger : MonoBehaviour
+{
+    //内部变量
+    private BoxCollider boxCollider;
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+       
+    }
+}
